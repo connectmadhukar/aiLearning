7b3e74e9cb081b0b71af1e68a50df6222b4c24f8
connectmadhukar <connectmadhukar@gmail.com>
2024-06-27 10:33:05 -0700

    Initializing the project

---
+++ src/test/java/com/mm/ai/learning/AiLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.ai.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
